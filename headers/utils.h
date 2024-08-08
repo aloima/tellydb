2938ed02b8e0794c686c03208ffe19fe126d4c4a
aloima <acar.aloima@gmail.com>
2024-08-08 19:00:50 +0300

    feat: add set_string method

--- headers/utils.h
+++ headers/utils.h
@@ -1,10 +1,13 @@
 #include <stddef.h>
+#include <stdint.h>
 
 #ifndef UTILS_H
   #define UTILS_H
 
   typedef struct String {
-    char *data;
+    char *value;
     size_t len;
   } string_t;
+
+  void set_string(string_t *data, char *value, int32_t len);
 #endif
---
+++ src/utils/string.c
@@ -0,0 +1,17 @@
+#include "../../headers/telly.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+
+void set_string(string_t *data, char *value, int32_t len) {
+  data->len = len == -1 ? strlen(value) : len;
+  const uint32_t size = data->len + 1;
+
+  if (data->value != NULL) {
+    data->value = realloc(data->value, size);
+  } else {
+    data->value = malloc(size);
+  }
+
+  memcpy(data->value, value, size);
+}
