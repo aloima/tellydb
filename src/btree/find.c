80bd00caec70c2a3bd9b27afb317aa01837f87f3
aloima <acar.aloima@gmail.com>
2024-08-24 01:00:21 +0300

    fix: add NULL node and node->data problem when finding data from btree

--- src/btree/find.c
+++ src/btree/find.c
@@ -1,30 +1,32 @@
 #include "../../headers/telly.h"
 
 static struct KVPair *find_kv_from_node(struct BTreeNode *node, const char *key) {
-  const char *first = node->data[0]->key.value;
-  const char c = key[0];
+  if (node != NULL && node->data != NULL) {
+    const char *first = node->data[0]->key.value;
+    const char c = key[0];
 
-  if (streq(first, key)) {
-    return node->data[0];
-  } else if (node->leaf_count != 0 && c <= first[0]) {
-    return find_kv_from_node(node->leafs[0], key);
-  } else if (node->leaf_count != 0) {
-    for (uint32_t i = 0; i < node->size; ++i) {
-      struct KVPair *pair = node->data[i];
-      const char *pair_key = pair->key.value;
+    if (streq(first, key)) {
+      return node->data[0];
+    } else if (node->leaf_count != 0 && c <= first[0]) {
+      return find_kv_from_node(node->leafs[0], key);
+    } else if (node->leaf_count != 0) {
+      for (uint32_t i = 0; i < node->size; ++i) {
+        struct KVPair *pair = node->data[i];
+        const char *pair_key = pair->key.value;
 
-      if (streq(key, pair_key)) {
-        return pair;
-      } else if (c <= pair_key[0]) {
-        return find_kv_from_node(node->leafs[i], key);
+        if (streq(key, pair_key)) {
+          return pair;
+        } else if (c <= pair_key[0]) {
+          return find_kv_from_node(node->leafs[i], key);
+        }
       }
-    }
-  } else {
-    for (uint32_t i = 1; i < node->size; ++i) {
-      struct KVPair *pair = node->data[i];
+    } else {
+      for (uint32_t i = 1; i < node->size; ++i) {
+        struct KVPair *pair = node->data[i];
 
-      if (streq(pair->key.value, key)) {
-        return pair;
+        if (streq(pair->key.value, key)) {
+          return pair;
+        }
       }
     }
   }
