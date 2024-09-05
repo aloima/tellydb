256a4025e9084a9a6a0ecc87f6b63a46bf87a879
aloima <acar.aloima@gmail.com>
2024-09-05 23:04:24 +0300

    chore(btree): add missing stdint header

--- src/btree/delete.c
+++ src/btree/delete.c
@@ -1,17 +1,19 @@
 #include "../../headers/telly.h"
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 void del_kv_from_node(struct BTreeNode *node, char *key) {
   for (uint32_t i = 0; i < node->size; ++i) {
-    if (streq(node->data[i]->key.value, key)) {
-      struct KVPair *del_addr = node->data[i];
+    struct KVPair *pair = node->data[i];
+
+    if (streq(pair->key.value, key)) {
       memcpy(node->data + i, node->data + i + 1, (node->size - i - 1) * sizeof(struct KVPair *));
 
-      free(del_addr->key.value);
-      if (del_addr->type == TELLY_STR) free(del_addr->value.string.value);
-      free(del_addr);
+      free(pair->key.value);
+      if (pair->type == TELLY_STR) free(pair->value.string.value);
+      free(pair);
 
       node->size -= 1;
       node->data = realloc(node->data, node->size * sizeof(struct KVPair *));
