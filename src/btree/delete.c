062f9aa1e1dc0c090211c1e1fd4afca9f174293f
aloima <acar.aloima@gmail.com>
2024-09-29 11:47:43 +0300

    perf(btree): same conditions in del_kv_from_node

--- src/btree/delete.c
+++ src/btree/delete.c
@@ -3,23 +3,25 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void del_kv_from_node(struct BTreeNode *node, char *key) {
+void del_kv_from_node(struct BTreeNode *node, const char *key) {
   const uint32_t at = find_index_of_kv(node, key);
   if (at >= node->size) return;
 
-  struct KVPair *pair = node->data[at];
+  struct KVPair *kv = node->data[at];
 
-  if (node->size == 1 && streq(pair->key.value, key)) {
-    free_kv(pair);
-    free(node->data);
+  if (streq(kv->key.value, key)) {
+    if (node->size == 1) {
+      free_kv(kv);
+      free(node->data);
 
-    node->data = NULL;
-    node->size = 0;
-  } else if (streq(pair->key.value, key)) {
-    move_kv(node, at, node->size - 1);
-    free_kv(pair);
+      node->data = NULL;
+      node->size = 0;
+    } else {
+      move_kv(node, at, node->size - 1);
+      free_kv(kv);
 
-    node->size -= 1;
-    node->data = realloc(node->data, node->size * sizeof(struct KVPair *));
+      node->size -= 1;
+      node->data = realloc(node->data, node->size * sizeof(struct KVPair *));
+    }
   }
 }
