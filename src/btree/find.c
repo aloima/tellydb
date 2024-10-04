76e7378bf78077cd5d52f7197f08d935300631bc
aloima <acar.aloima@gmail.com>
2024-10-04 17:48:25 +0300

    fix(database): faulty find_kv_from_node method

--- src/btree/find.c
+++ src/btree/find.c
@@ -3,15 +3,14 @@
 #include <stddef.h>
 
 static struct KVPair *find_kv_from_node(struct BTreeNode *node, const char *key) {
-  const char c = key[0];
-
   if (node->leafs != NULL) {
     for (uint32_t i = 0; i < node->size; ++i) {
       struct KVPair *kv = node->data[i];
       const char *kv_key = kv->key->value;
+      const int search = strcmp(key, kv_key);
 
-      if (streq(key, kv_key)) return kv;
-      else if (c <= kv_key[0]) return find_kv_from_node(node->leafs[i], key);
+      if (search == 0) return kv;
+      else if (search < 0) return find_kv_from_node(node->leafs[i], key);
     }
 
     return find_kv_from_node(node->leafs[node->size], key);
