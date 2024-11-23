1e79bf00be2b8ce8b60f49cb4d4dd3bff078be42
aloima <acar.aloima@gmail.com>
2024-11-23 21:56:50 +0300

    feat: add apple-specific code to open_file method

--- src/utils/file.c
+++ src/utils/file.c
@@ -8,11 +8,17 @@
 int open_file(const char *file, int flags) {
   int fd;
 
+#if defined(__linux__)
   if ((fd = open(file, (O_RDWR | O_CREAT | O_DIRECT | flags), (S_IRUSR | S_IWUSR))) == -1) {
+#else
+  if ((fd = open(file, (O_RDWR | O_CREAT | flags), (S_IRUSR | S_IWUSR))) == -1) {
+#endif
     switch (errno) {
+    #if defined(__linux__)
       case EINVAL:
         write_log(LOG_ERR, "Direct I/O does not be supported by your file system, cannot open file.");
         break;
+    #endif
 
       case EISDIR:
         write_log(LOG_ERR, "Specified file is a directory, cannot open file.");
@@ -32,6 +38,26 @@ int open_file(const char *file, int flags) {
 
     return -1;
   }
+
+  #if defined(__APPLE__)
+  if ((fcntl(fd, F_NOCACHE, 1)) == -1) {
+    switch (errno) {
+      case EACCES:
+        write_log(LOG_ERR, "File descriptor is not accessible to set for no kernel caching mode.");
+        break;
+
+      default:
+        write_log(LOG_ERR, "File descriptor cannot be set for no kernel caching mode.");
+    }
+
+    close(file);
+    return -1;
+  }
+  #endif
+
+  #if (!defined(__APPLE__) && !defined(__linux__))
+  write_log(LOG_WARN, "File descriptor cannot be opened as no kernel caching mode or Direct I/O. Use MacOS or Linux.");
+  #endif
 
   return fd;
 }
