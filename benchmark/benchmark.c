275b6600856c94db1e57a865fe16f28e280179d0
aloima <acar.aloima@gmail.com>
2025-02-07 23:53:52 +0300

    refactor: update benchmark utility

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
-#include <time.h>
 
 #include <hiredis/hiredis.h>
 
-int main() {
-  redisContext *c;
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    puts("write ip and port");
+    return 0;
+  }
 
-  c = redisConnect("127.0.0.1", 6379);
+  printf("ip: %s\nport: %s\n", argv[1], argv[2]);
+
+  redisContext *c = redisConnect(argv[1], atoi(argv[2]));
 
   if (c->err) {
     printf("error: %s\n", c->errstr);
     return 1;
   }
 
-  clock_t start_time = clock();
-  uint32_t ops = 0;
-
-  for (uint32_t i = 0; i < 1e7; ++i) {
-    freeReplyObject(redisCommand(c,"GET string"));
-    ops += 1;
+  for (uint32_t i = 0; i < 100; ++i) {
+    freeReplyObject(redisCommand(c, "SET %d %d-value", i, i));
   }
-
-  double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
-  printf("Done in %f seconds, %f ops/sec\n", elapsed_time, (ops / elapsed_time));
 
   redisFree(c);
 }
