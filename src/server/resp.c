753f257b29babd56d44701c3c4453524277916e7
aloima <acar.aloima@gmail.com>
2025-09-10 18:00:44 +0300

    fix(resp): invalid taking excessive buffer
    * Improve error handling of getting command data

--- src/server/resp.c
+++ src/server/resp.c
@@ -8,22 +8,16 @@
 #define DATA_ERR(client) \
   write_log(LOG_ERR, "Received data from Client #%u cannot be validated as a RESP data, so it cannot be created as a command.", (client)->id);
 
-#define TAKE_BYTES_RETURN_VALUE(value, n, return_value) \
+#define TAKE_BYTES(value, n, return_value) \
   if (take_n_bytes(client, buf, at, value, n, size) != n) { \
     DATA_ERR(client); \
     return return_value; \
   }
 
-#define TAKE_BYTES(value, n) \
-  if (take_n_bytes(client, buf, at, value, n, size) != n) { \
-    DATA_ERR(client); \
-    return; \
-  }
-
 static inline int take_n_bytes(struct Client *client, char *buf, int32_t *at, void *data, const uint32_t n, int32_t *size) {
   const uint32_t remaining = (RESP_BUF_SIZE - *at);
 
-  if (VERY_LIKELY(*at < remaining)) {
+  if (VERY_LIKELY(n < remaining)) {
     if (data != NULL) {
       memcpy_aligned(data, buf + *at, n);
     }
@@ -38,7 +32,15 @@ static inline int take_n_bytes(struct Client *client, char *buf, int32_t *at, vo
     if (_read(client, data + remaining, n - remaining) <= 0) {
       return remaining;
     }
+
+    // Needs to read buffer, because the process may be interrupted in the middle of getting command data
+    *size = _read(client, buf, RESP_BUF_SIZE);
+
+    if (*size == -1) {
+      return -1;
+    }
   } else {
+    // TODO: fix dummy reading/add buffer
     char dummy[128];
     uint32_t length = n - remaining;
     uint32_t quotient = length / 128;
@@ -48,100 +50,100 @@ static inline int take_n_bytes(struct Client *client, char *buf, int32_t *at, vo
     }
 
     _read(client, dummy, length - (128 * quotient));
+    *size = 0;
   }
 
   *at = 0;
-  *size = 0;
   return n;
 }
 
-static inline void get_resp_command_name(struct Client *client, commandname_t *name, char *buf, int32_t *at, int32_t *size) {
+static inline bool get_resp_command_name(struct Client *client, commandname_t *name, char *buf, int32_t *at, int32_t *size) {
   char c;
-  TAKE_BYTES(&c, 1);
+  TAKE_BYTES(&c, 1, false);
 
   if (VERY_UNLIKELY(c != RDT_BSTRING)) {
     DATA_ERR(client);
-    return;
+    return false;
   }
 
   while (true) {
-    TAKE_BYTES(&c, 1);
+    TAKE_BYTES(&c, 1, false);
 
     if (isdigit(c)) {
       name->len = (name->len * 10) + (c - 48);
     } else if (c == '\r') {
-      TAKE_BYTES(&c, 1);
+      TAKE_BYTES(&c, 1, false);
 
       if (c != '\n') {
         DATA_ERR(client);
-        return;
+        return false;
       }
 
       if (name->len > COMMAND_NAME_MAX_LENGTH) {
-        TAKE_BYTES(name->value, COMMAND_NAME_MAX_LENGTH - 1);
+        TAKE_BYTES(name->value, COMMAND_NAME_MAX_LENGTH - 1, false);
         name->value[name->len] = '\0';
 
-        TAKE_BYTES(NULL, name->len - (COMMAND_NAME_MAX_LENGTH - 1));
+        TAKE_BYTES(NULL, name->len - (COMMAND_NAME_MAX_LENGTH - 1), false);
       } else {
-        TAKE_BYTES(name->value, name->len);
+        TAKE_BYTES(name->value, name->len, false);
         name->value[name->len] = '\0';
       }
 
       char crlf[2];
-      TAKE_BYTES(crlf, 2);
+      TAKE_BYTES(crlf, 2, false);
 
       if (VERY_UNLIKELY(crlf[0] != '\r' || crlf[1] != '\n')) {
         DATA_ERR(client);
-        return;
+        return false;
       }
 
-      return;
+      return true;
     } else {
       DATA_ERR(client);
-      return;
+      return false;
     }
   }
 }
 
-static inline void get_resp_command_argument(struct Client *client, string_t *argument, char *buf, int32_t *at, int32_t *size) {
+static inline bool get_resp_command_argument(struct Client *client, string_t *argument, char *buf, int32_t *at, int32_t *size) {
   char c;
-  TAKE_BYTES(&c, 1);
+  TAKE_BYTES(&c, 1, false);
 
   if (VERY_UNLIKELY(c != RDT_BSTRING)) {
     DATA_ERR(client);
-    return;
+    return false;
   }
 
   while (true) {
-    TAKE_BYTES(&c, 1);
+    TAKE_BYTES(&c, 1, false);
 
     if (isdigit(c)) {
       argument->len = (argument->len * 10) + (c - 48);
     } else if (c == '\r') {
-      TAKE_BYTES(&c, 1);
+      TAKE_BYTES(&c, 1, false);
 
       if (c != '\n') {
         DATA_ERR(client);
-        return;
+        return false;
       }
 
       argument->value = malloc(argument->len + 1);
-      TAKE_BYTES(argument->value, argument->len);
+      TAKE_BYTES(argument->value, argument->len, false);
       argument->value[argument->len] = '\0';
 
-      char crlf[2];
-      TAKE_BYTES(crlf, 2);
+      char crlf[2] = {0, 0};
+      TAKE_BYTES(crlf, 2, false);
 
       if (VERY_UNLIKELY(crlf[0] != '\r' || crlf[1] != '\n')) {
         DATA_ERR(client);
         free(argument->value);
-        return;
+        return false;
       }
 
-      return;
+      return true;
     } else {
       DATA_ERR(client);
-      return;
+      return false;
     }
   }
 }
@@ -152,12 +154,12 @@ static bool parse_resp_command(struct Client *client, char *buf, int32_t *at, in
 
   while (true) {
     char c;
-    TAKE_BYTES_RETURN_VALUE(&c, 1, false);
+    TAKE_BYTES(&c, 1, false);
 
     if (isdigit(c)) {
       command->arg_count = (command->arg_count * 10) + (c - 48);
     } else if (c == '\r') {
-      TAKE_BYTES_RETURN_VALUE(&c, 1, false);
+      TAKE_BYTES(&c, 1, false);
 
       if (c != '\n') {
         DATA_ERR(client);
@@ -170,7 +172,11 @@ static bool parse_resp_command(struct Client *client, char *buf, int32_t *at, in
       }
 
       command->name.len = 0;
-      get_resp_command_name(client, &command->name, buf, at, size);
+
+      if (!get_resp_command_name(client, &command->name, buf, at, size)) {
+        return false;
+      }
+
       command->arg_count -= 1;
 
       if (command->arg_count != 0) {
@@ -178,7 +184,10 @@ static bool parse_resp_command(struct Client *client, char *buf, int32_t *at, in
 
         for (uint32_t i = 0; i < command->arg_count; ++i) {
           command->args[i].len = 0;
-          get_resp_command_argument(client, &command->args[i], buf, at, size);
+
+          if (!get_resp_command_argument(client, &command->args[i], buf, at, size)) {
+            return false;
+          }
         }
       }
 
@@ -192,7 +201,7 @@ static bool parse_resp_command(struct Client *client, char *buf, int32_t *at, in
 
 bool get_command_data(struct Client *client, char *buf, int32_t *at, int32_t *size, commanddata_t *command) {
   uint8_t type;
-  TAKE_BYTES_RETURN_VALUE(&type, 1, false);
+  TAKE_BYTES(&type, 1, false);
 
   if (VERY_LIKELY(type == RDT_ARRAY)) {
     if (parse_resp_command(client, buf, at, size, command)) {
