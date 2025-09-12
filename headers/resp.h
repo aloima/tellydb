e00ea46df89facbef4811983c9b31901d814b8a9
aloima <acar.aloima@gmail.com>
2025-09-13 00:58:37 +0300

    feat: rwrite create_resp_integer_mpz for mpf_t

--- headers/resp.h
+++ headers/resp.h
@@ -3,6 +3,7 @@
 #include "server/server.h"
 #include "utils.h"
 
+#include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -20,6 +21,12 @@
 #define RDT_INTEGER ':'
 #define RDT_INTEGER_SL ":"
 
+#define RDT_BIGNUMBER '('
+#define RDT_BIGNUMBER_SL "("
+
+#define RDT_DOUBLE ','
+#define RDT_DOUBLE_SL ","
+
 #define RDT_ERR '-'
 #define RDT_ERR_SL "-"
 
@@ -34,15 +41,82 @@ static inline int create_resp_integer(char *buf, uint64_t value) {
   return (nbytes + 3);
 }
 
+// TODO: "Invalid response type 13" from redis-cli on RDT_BIGNUMBER
+static inline int create_resp_integer_mpf(const enum ProtocolVersion protover, char *buf, mpf_t value) {
+  int nbytes = 1;
 
-static inline int create_resp_integer_mpz(char *buf, mpz_t value) {
-  *(buf) = RDT_INTEGER;
-  mpz_get_str(buf + 1, 10, value);
+  mp_exp_t exp;
+  char *str = mpf_get_str(NULL, &exp, 10, 0, value);
+  const uint64_t len = strlen(str);
+  bool zeroed = true;
 
-  const uint64_t nbytes = strlen(buf) - 1;
-  *(buf + nbytes + 1) = '\r';
-  *(buf + nbytes + 2) = '\n';
-  return (nbytes + 3);
+  if (len == 0) {
+    switch (protover) {
+      case RESP2:
+        *(buf) = RDT_INTEGER;
+        break;
+
+      case RESP3:
+        *(buf) = RDT_BIGNUMBER;
+        break;
+    }
+
+    *(buf + 1) = '0';
+    *(buf + 2) = '\r';
+    *(buf + 3) = '\n';
+    return 4;
+  }
+
+  if (str[0] == '-') {
+    exp += 1;
+  }
+
+  for (uint64_t i = 0; i < len; ++i) {
+    if (i == exp) {
+      buf[nbytes++] = '.';
+    }
+
+    buf[nbytes++] = str[i];
+
+    if (exp < i && zeroed && str[i] != 0) {
+      zeroed = false;
+    }
+  }
+
+  if (zeroed) {
+    nbytes = ((nbytes < exp) ? nbytes : exp);
+
+    if (mpf_fits_slong_p(value) == 0) {
+      switch (protover) {
+        case RESP2:
+          // TODO: string representation
+          break;
+
+        case RESP3:
+          *(buf) = RDT_BIGNUMBER;
+          break;
+      }
+    } else {
+      *(buf) = RDT_INTEGER;
+    }
+  } else {
+    switch (protover) {
+      case RESP2:
+        nbytes = ((nbytes < exp) ? nbytes : exp);
+        *(buf) = RDT_INTEGER;
+        break;
+
+      case RESP3:
+        *(buf) = RDT_DOUBLE;
+        break;
+    }
+  }
+
+  *(buf + nbytes++) = '\r';
+  *(buf + nbytes++) = '\n';
+  free(str);
+
+  return nbytes;
 }
 
 static inline uint64_t create_resp_string(char *buf, string_t string) {
