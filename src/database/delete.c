d39e522fbd2b8b14ad5061518f71e067e1b99fee
aloima <acar.aloima@gmail.com>
2025-09-20 15:53:17 +0300

    fix: add key comparing to delete_data method

--- src/database/delete.c
+++ src/database/delete.c
@@ -6,8 +6,22 @@
 
 bool delete_data(struct Database *database, const string_t key) {
   const uint64_t capacity = database->size.capacity;
-  const uint64_t index = (hash((char *) key.value, key.len) % capacity);
-  struct KVPair *kv = database->data[index];
+  uint64_t index = (hash((char *) key.value, key.len) % capacity);
+  struct KVPair *kv;
+
+  while ((kv = database->data[index])) {
+    if ((kv->hashed % capacity) != index) {
+      return false;
+    } else if ((kv->key.len == key.len) && (memcmp(kv->key.value, key.value, key.len) == 0)) {
+      break;
+    }
+
+    index += 1;
+
+    if (index == capacity) {
+      return false;
+    }
+  }
 
   if (!kv) {
     return false;
@@ -16,18 +30,18 @@ bool delete_data(struct Database *database, const string_t key) {
   free_kv(kv);
 
   for (uint64_t i = (index + 1); i < capacity; ++i) {
-    struct KVPair *move = database->data[i];
+    struct KVPair *pair = database->data[i];
 
-    if (!move) {
+    if (!pair) {
       database->size.stored -= 1;
       database->data[i - 1] = NULL;
       return true;
     }
 
-    if (index == (move->hashed % capacity)) {
-      database->data[i - 1] = move;
+    if (index == (pair->hashed % capacity)) {
+      database->data[i - 1] = pair;
     }
   }
 
-  return true;
+  return false;
 }
