7aa11ec4f8bc8bcce0755da8016d096c140589fb
aloima <acar.aloima@gmail.com>
2025-09-21 21:36:37 +0300

    fix: indexing on del_field_from_hashtable method

--- src/hashtable/memory.c
+++ src/hashtable/memory.c
@@ -42,35 +42,47 @@ void set_field_of_hashtable(struct HashTable *table, const string_t name, void *
   field->hash = hashed;
 }
 
+static inline uint32_t add_to_index(const uint32_t index, const uint32_t capacity) {
+  return ((index + 1) % capacity);
+}
+
 bool del_field_to_hashtable(struct HashTable *table, const string_t name) {
   const uint32_t capacity = table->size.capacity;
   const uint64_t hashed = hash(name.value, name.len);
-  uint32_t index = hashed % capacity;
+  const uint32_t start_idx = (hashed % capacity);
+  uint32_t index = start_idx;
 
   struct HashTableField *field;
 
-  while ((field = table->fields[index])) {
+  while ((field = table->fields[index]) && (field->hash == index)) {
     if ((name.len != field->name.len) || (memcmp(field->name.value, name.value, name.len) != 0)) {
-      index = ((index + 1) % capacity);
+      index = add_to_index(index, capacity);
+
+      if (index == start_idx) {
+        break;
+      }
+
       continue;
     }
 
     const uint32_t size = (capacity * HASHTABLE_SHRINK_FACTOR);
     table->size.used -= 1;
+    table->fields[index] = NULL; // when shrinked, table capacity may be 1. so, loop may be not executed.
 
     if (size == table->size.used) {
       table->fields[index] = NULL;
       resize_hashtable(table, size);
     } else {
-      for (uint32_t i = (index + 1); i < capacity; ++i) {
+      for (uint32_t i = add_to_index(index, capacity); i != index; (i = add_to_index(index, capacity))) {
         struct HashTableField *move = table->fields[i];
+        const uint32_t prev = ((i == 0) ? (capacity - 1) : (i - 1));
 
         if (!move || (move->hash != field->hash)) {
-          table->fields[i - 1] = NULL; // table->fields[index] will be processed for valid situation
+          table->fields[prev] = NULL;
           break;
         }
 
-        table->fields[i - 1] = move;
+        table->fields[prev] = move;
       }
     }
 
