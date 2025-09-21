7b338ddc10de3cb14fee39709db02c5ff7062f3a
acar <83824840+aloima@users.noreply.github.com>
2025-09-21 21:44:27 +0300

    fix: indexing problem of deleting methods from hashtable (#23)
    * fix: indexing and deleting on delete_data method
    
    * fix: finding index on delete_data method
    
    * fix: indexing on del_field_from_hashtable method

--- src/database/delete.c
+++ src/database/delete.c
@@ -4,9 +4,14 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+static inline uint64_t add_to_index(const uint64_t index, const uint64_t capacity) {
+  return ((index + 1) % capacity);
+}
+
 bool delete_data(struct Database *database, const string_t key) {
   const uint64_t capacity = database->size.capacity;
-  uint64_t index = (hash((char *) key.value, key.len) % capacity);
+  const uint64_t start_idx = (hash((char *) key.value, key.len) % capacity);
+  uint64_t index = index;
   struct KVPair *kv;
 
   while ((kv = database->data[index])) {
@@ -16,32 +21,33 @@ bool delete_data(struct Database *database, const string_t key) {
       break;
     }
 
-    index += 1;
+    index = add_to_index(index, capacity);
 
-    if (index == capacity) {
+    if (index == start_idx) {
       return false;
     }
   }
 
-  if (!kv) {
-    return false;
-  }
-
   free_kv(kv);
+  database->data[index] = NULL; // Needs it for uncollised indexes and filled next index
 
-  for (uint64_t i = (index + 1); i < capacity; ++i) {
+  for (uint64_t i = add_to_index(index, capacity); i != index; i = add_to_index(index, capacity)) {
     struct KVPair *pair = database->data[i];
+    const uint64_t prev = ((i == 0) ? (capacity - 1) : (i - 1));
 
     if (!pair) {
       database->size.stored -= 1;
-      database->data[i - 1] = NULL;
-      return true;
+      database->data[prev] = NULL;
+      break;
     }
 
+    // On collised index
     if (index == (pair->hashed % capacity)) {
-      database->data[i - 1] = pair;
+      database->data[prev] = pair;
+    } else {
+      break;
     }
   }
 
-  return false;
+  return true;
 }
