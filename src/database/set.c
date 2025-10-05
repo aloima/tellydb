035cc6b37596907f6af0f978c89c9351dbb127e1
aloima <acar.aloima@gmail.com>
2025-10-05 20:53:14 +0300

    perf(database): change 75% limit for resizing
    * It makes better CPU caching

--- src/database/set.c
+++ src/database/set.c
@@ -5,6 +5,14 @@
 
 #include <gmp.h>
 
+static inline uint64_t probe(struct KVPair **data, uint64_t index, const uint64_t capacity) {
+  while (data[index]) {
+    index = (index + 1) % capacity;
+  }
+
+  return index;
+}
+
 struct KVPair *set_data(struct Database *database, struct KVPair *data, const string_t key, void *value, const enum TellyTypes type) {
   if (data) {
     free_value(data->type, data->value);
@@ -14,6 +22,7 @@ struct KVPair *set_data(struct Database *database, struct KVPair *data, const st
     return data;
   } else {
     struct KVPair *kv = malloc(sizeof(struct KVPair));
+    const uint64_t old_capacity = database->size.capacity;
 
     if (!kv) {
       return NULL;
@@ -21,34 +30,32 @@ struct KVPair *set_data(struct Database *database, struct KVPair *data, const st
 
     set_kv(kv, key, value, type);
 
-    if (database->size.capacity == database->size.stored) {
-      struct KVPair **nd = calloc(database->size.capacity * 2, sizeof(struct KVPair *));
+    if ((old_capacity * 3) == (database->size.stored * 4)) { // If 75% of table is filled
+      struct KVPair **nd = calloc(old_capacity * 2, sizeof(struct KVPair *));
 
       if (!nd) {
         return NULL;
       }
 
-      database->size.capacity *= 2;
+      const uint64_t new_capacity = (database->size.capacity *= 2);
 
-      for (uint64_t i = 0; i < database->size.stored; ++i) {
-        // No need for NULL checking, because database is fulled already, all data is available.
+      for (uint64_t i = 0; i < old_capacity; ++i) {
         struct KVPair *current = database->data[i];
-        const uint64_t index = (current->hashed % database->size.capacity);
 
-        nd[index] = current;
+        if (current) {
+          const uint64_t index = (current->hashed % new_capacity);
+          nd[index] = current;
+        }
       }
 
       free(database->data);
       database->data = nd;
     }
 
-    uint64_t index = (kv->hashed % database->size.capacity);
+    const uint64_t new_capacity = database->size.capacity;
+    const uint64_t index = (kv->hashed % new_capacity);
 
-    while (database->data[index]) {
-      index = ((index + 1) % database->size.capacity);
-    }
-
-    database->data[index] = kv;
+    database->data[probe(database->data, index, new_capacity)] = kv;
     database->size.stored += 1;
 
     return kv;
