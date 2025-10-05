32e6f7bbb0f000e83603ab25c23b911606bde860
aloima <acar.aloima@gmail.com>
2025-10-05 23:11:15 +0300

    fix(database): uninitialized index on delete_data

--- src/database/delete.c
+++ src/database/delete.c
@@ -11,7 +11,7 @@ static inline uint64_t add_to_index(const uint64_t index, const uint64_t capacit
 bool delete_data(struct Database *database, const string_t key) {
   const uint64_t capacity = database->size.capacity;
   const uint64_t start_idx = (hash((char *) key.value, key.len) % capacity);
-  uint64_t index = index;
+  uint64_t index = start_idx;
   struct KVPair *kv;
 
   while ((kv = database->data[index])) {
