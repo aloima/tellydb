dcca33e79a65ed81dbbad8490be99b14fc60ebd8
aloima <acar.aloima@gmail.com>
2025-12-15 23:09:39 +0300

    perf(io): improve prefetching variables on threads

--- src/io/requests.c
+++ src/io/requests.c
@@ -24,7 +24,13 @@ void *handle_io_requests(void *arg) {
     Client *client = op.client;
     enum ClientState expected = CLIENT_STATE_ACTIVE;
 
-    __builtin_prefetch(client, 1, 3);
+    __builtin_prefetch(thread, 0, 0); // read_command reads, once reading
+    __builtin_prefetch(client, 0, 3); // read_command and _write reads
+    __builtin_prefetch(&client->id, 1, 0); // terminate_connection writes once
+
+    // It is atomic variable, taking it to CPU cache may be dangerous
+    // __builtin_prefetch(&client->state, 1, 3); // terminate_connection writes once
+    __builtin_prefetch(&op.write_str, 0, 1); // _write reads twice/three times
 
     while (!ATOMIC_CAS_WEAK(&client->state, &expected, CLIENT_STATE_PASSIVE, memory_order_acq_rel, memory_order_relaxed)) {
       if (expected == CLIENT_STATE_EMPTY) goto TERMINATION;
@@ -43,7 +49,7 @@ void *handle_io_requests(void *arg) {
 
       case IOOP_WRITE: {
         string_t *write_str = &op.write_str;
-        _write(op.client, op.write_str.value, op.write_str.len);
+        _write(client, op.write_str.value, op.write_str.len);
         break;
       }
 
