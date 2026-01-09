994ce3ac2bac7dce98f5deecaa086b4fc6b602fd
aloima <acar.aloima@gmail.com>
2026-01-10 00:44:12 +0300

    refactor/perf: handle_io_requests method

--- src/io/requests.c
+++ src/io/requests.c
@@ -15,74 +15,66 @@ void add_io_request(const enum IOOpType type, Client *client, string_t write_str
   sem_post(io_stored_sem);
 }
 
+static inline bool acquire_client_passive(Client *client) {
+  _Atomic(enum ClientState) *state = &client->state;
+
+  enum ClientState expected = CLIENT_STATE_ACTIVE;
+  int spin_count = 0;
+
+  while (!ATOMIC_CAS_WEAK(state, &expected, CLIENT_STATE_PASSIVE, memory_order_acq_rel, memory_order_relaxed)) {
+    if (expected == CLIENT_STATE_EMPTY) return false;
+
+    while (expected == CLIENT_STATE_PASSIVE) {
+      cpu_relax();
+
+      if (++spin_count > 1000) {
+        sched_yield();
+        spin_count = 0;
+      }
+
+      expected = atomic_load_explicit(state, memory_order_relaxed);
+      if (expected == CLIENT_STATE_EMPTY) return false;
+    }
+
+    expected = CLIENT_STATE_ACTIVE;
+  }
+
+  return true;
+}
+
 void *handle_io_requests(void *arg) {
   IOThread *thread = arg;
 
-  while (atomic_load_explicit(&thread->status, memory_order_acquire) == ACTIVE) {
+  while (atomic_load_explicit(&thread->status, memory_order_relaxed) == ACTIVE) {
     sem_wait(io_stored_sem);
     IOOperation op;
 
     if (!pop_tqueue(io_queue, &op)) continue;
     sem_post(io_available_space_sem);
-    thread = arg;
 
     Client *client = op.client;
     enum ClientState expected = CLIENT_STATE_ACTIVE;
 
-    __builtin_prefetch(thread, 0, 0); // read_command reads, once reading
-    __builtin_prefetch(client, 0, 3); // read_command and _write reads
-    __builtin_prefetch(&client->id, 1, 0); // terminate_connection writes once
-
-    // It is atomic variable, taking it to CPU cache may be dangerous
-    // __builtin_prefetch(&client->state, 1, 3); // terminate_connection writes once
-    __builtin_prefetch(&op.write_str, 0, 1); // _write reads twice/three times
-
-    _Atomic(enum ClientState) *state = &client->state;
-
-    while (true) {
-      if (ATOMIC_CAS_WEAK(state, &expected, CLIENT_STATE_PASSIVE, memory_order_acq_rel, memory_order_relaxed)) {
-        break;
-      }
-
-      if (expected == CLIENT_STATE_EMPTY) goto TERMINATION;
-      int spin_count = 0;
-
-      while (1) {
-        enum ClientState current_state = atomic_load_explicit(state, memory_order_relaxed);
-        if (current_state == CLIENT_STATE_ACTIVE) break;
-        if (current_state == CLIENT_STATE_EMPTY) goto TERMINATION;
-        cpu_relax();
-
-        if (++spin_count > 1000) {
-          sched_yield();
-          spin_count = 0;
-        }
-      }
-
-      expected = CLIENT_STATE_ACTIVE;
-    }
+    __builtin_prefetch(client, 0, 1);
+    if (!acquire_client_passive(client)) continue;
 
     switch (op.type) {
       case IOOP_GET_COMMAND:
         read_command(thread, client);
+        atomic_store_explicit(&client->state, CLIENT_STATE_ACTIVE, memory_order_release);
         break;
 
       case IOOP_TERMINATE:
         terminate_connection(client);
-        goto TERMINATION;
         break;
 
       case IOOP_WRITE: {
         string_t *write_str = &op.write_str;
         _write(client, op.write_str.value, op.write_str.len);
+        atomic_store_explicit(&client->state, CLIENT_STATE_ACTIVE, memory_order_release);
         break;
       }
     }
-
-    atomic_store_explicit(&client->state, CLIENT_STATE_ACTIVE, memory_order_release);
-
-TERMINATION:
-    (void) NULL;
   }
 
   if (thread->read_buf) free(thread->read_buf);
