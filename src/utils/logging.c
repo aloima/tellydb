8e9b19c1aaa31f2e9cdc58f74da967a22b85214d
aloima <acar.aloima@gmail.com>
2026-01-13 13:58:24 +0300

    feat(utils): use mmap() in logging

--- src/utils/logging.c
+++ src/utils/logging.c
@@ -6,109 +6,68 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <errno.h>
 #include <time.h>
 
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 
-static Config *_conf = NULL;
 static int fd = -1;
-
-// Also, use it as default maximum log length
-static uint16_t block_size = 4096;
+static off_t new_size = 0;
+#define LOG_LENGTH 4096
 
 struct ThreadQueue *lines;
 
+// TODO: handle max_log_liens == -1
 bool initialize_logs() {
-  _conf = server->conf;
-  if ((fd = open_file(_conf->log_file, 0)) == -1) return false;
+  const Config *conf = server->conf;
+  if (conf->log_file[0] == '\0') return true;
+  if ((fd = open_file(conf->log_file, 0)) == -1) return false;
 
   struct stat sostat;
-  stat(_conf->log_file, &sostat);
+  stat(conf->log_file, &sostat);
 
-  const off_t file_size = sostat.st_size;
-  block_size = sostat.st_blksize;
+  const off_t size = sostat.st_size;
+  new_size += size;
+  lines = create_tqueue(conf->max_log_lines * 2, sizeof(char *), alignof(typeof(char *)));
 
-  const uint32_t size = (block_size + 41);
+  if (size != 0) {
+    char *data = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    off_t old_at = 0;
+    off_t at = 0;
 
-  if (_conf->max_log_lines != -1) {
-    const uint32_t max_log_size = (block_size + 1);
-    lines = create_tqueue(_conf->max_log_lines * 2, sizeof(char *), _Alignof(char *));
-
-    if (file_size != 0) {
-      char *block;
-      if (posix_memalign((void **) &block, block_size, block_size) != 0) return false;
-
-      uint16_t latest = 0;
-      uint16_t count = 0;
-      char *buf = malloc(size);
-
-      while ((count = read(fd, block, block_size))) {
-        uint32_t i = 0;
-
-INSIDE_LOOP:
-        while (i < count) {
-          const char c = block[i];
-
-          if (c == '\n') {
-            memcpy(buf, (block + latest), (i - latest + 1));
-            buf[i - latest + 1] = '\0';
-            latest = i + 1;
-
-            push_tqueue(lines, &buf);
-            buf = malloc(size);
-          }
-
-          i += 1;
-        }
-
-        if (count == latest) {
-          latest = 0;
-        } else {
-          memcpy(buf, (block + latest), (count - latest));
-
-          const uint16_t old_count = count;
-          count = read(fd, block, block_size);
-          i = 0;
-
-          while (i < count) {
-            const char c = block[i];
-
-            if (c == '\n') {
-              memcpy(buf + (old_count - latest), block, (i + 1));
-              buf[old_count - latest + i + 1] = '\0';
-
-              push_tqueue(lines, &buf);
-              buf = malloc(size);
-
-              i += 1;
-              latest = i;
-              goto INSIDE_LOOP;
-            }
-
-            i += 1;
-          }
-        }
+    while (data[at] != '\0') {
+      if (data[at] != '\n') {
+        at += 1;
+        continue;
       }
 
-      free(buf);
-      free(block);
+      const off_t line_len = (at - old_at + 1); // line + '\n'
+      char *line = malloc(line_len + 1);
+      memcpy(line, data + old_at, line_len * sizeof(char));
+      line[line_len] = '\0';
+
+      push_tqueue(lines, &line);
+      at += 1;
+      old_at = at;
     }
+
+    munmap(data, size);
   }
 
   return true;
 }
 
 void write_log(enum LogLevel level, const char *fmt, ...) {
-  Config *conf = _conf ?: get_default_config();
+  Config *conf = server->conf ?: get_default_config();
   const uint8_t check = (conf->allowed_log_levels & level);
 
   char time_text[21];
   generate_date_string(time_text, time(NULL));
 
-  const uint32_t buf_size = block_size + 1;
+  const uint32_t buf_size = LOG_LENGTH + 1;
   char buf[buf_size];
   va_list args;
 
@@ -117,7 +76,7 @@ void write_log(enum LogLevel level, const char *fmt, ...) {
   va_end(args);
 
   uint32_t message_len = 0;
-  char message[block_size + 34];
+  char message[LOG_LENGTH + 34];
 
   FILE *stream;
 
@@ -149,10 +108,8 @@ void write_log(enum LogLevel level, const char *fmt, ...) {
   fputs(message, stream);
   if (fd == -1) return;
 
-  const uint32_t size = (block_size + 41);
-
   if (conf->max_log_lines == -1) {
-    char *line = malloc(size);
+    char *line = malloc(message_len + 1);
     memcpy(line, message, (message_len + 1));
 
     push_tqueue(lines, &line);
@@ -160,75 +117,69 @@ void write_log(enum LogLevel level, const char *fmt, ...) {
     if (estimate_tqueue_size(lines) >= conf->max_log_lines) {
       char *line;
       pop_tqueue(lines, &line);
+      new_size -= strlen(line);
       free(line);
     }
 
-    char *line = malloc(size);
+    char *line = malloc(message_len + 1);
     memcpy(line, message, (message_len + 1));
     push_tqueue(lines, &line);
+    new_size += message_len;
   }
 }
 
+#define CHECK_ERROR(ERROR_CODE, message) \
+  case (ERROR_CODE): \
+    fprintf(stderr, (message)); \
+    return
+
 void save_and_close_logs() {
-  int status = 0;
   if (fd == -1) return;
+  if (ftruncate(fd, new_size) == -1) {
+    if (errno == EIO) fprintf(stderr, "Cannot write logs to file, truncate I/O error.");
+    return;
+  }
 
-  char *buf;
-  if (posix_memalign((void **) &buf, block_size, block_size) != 0) goto CLEANUP;
-  memset(buf, 0, block_size);
-
-  off_t length = 0;
-  uint16_t at = 0;
-  lseek(fd, 0, SEEK_SET);
+  off_t written = 0;
+  char *map = mmap(NULL, new_size + 1, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (map == MAP_FAILED) {
+    switch (errno) {
+      CHECK_ERROR(ENOMEM, "Cannot write logs to file, out of memory.");
+      CHECK_ERROR(EAGAIN, "Cannot write logs to file, locked log file.");
+      CHECK_ERROR(ENFILE, "Cannot write logs to file, exceeded max opened files.");
+      CHECK_ERROR(ENODEV, "Cannot write logs to file, file system does not support memory mapping.");
+      CHECK_ERROR(EPERM,  "Cannot write logs to file, permission error.");
+    }
+  }
 
   while (estimate_tqueue_size(lines) != 0) {
     char *line;
-    pop_tqueue(lines, &line);
+    if (!pop_tqueue(lines, &line)) break;
 
     const uint32_t len = strlen(line);
 
-    if ((at + len) <= block_size) {
-      memcpy(buf + at, line, len);
-      at += len;
-    } else {
-      const uint16_t remaining = (block_size - at);
-      memcpy(buf + at, line, remaining);
-      length += write(fd, buf, block_size);
-
-      memcpy(buf, line + remaining, (at = (len - remaining)));
-    }
+    memcpy(map + written, line, len);
+    written += len;
 
     free(line);
   }
 
-  if (at != 0) {
-    if ((status = write(fd, buf, block_size)) == -1) {
-      free(buf);
-      write_log(LOG_WARN, "There was an error when writing logs to file.");
-      return;
-    }
-
-    free(buf);
-
-    if (status != -1 && (status = ftruncate(fd, length + at)) == -1) {
-      write_log(LOG_WARN, "There was an error when writing logs to file.");
-      return;
-    }
-  } else {
-    if ((status = ftruncate(fd, length)) == -1) {
-      free(buf);
-      write_log(LOG_WARN, "There was an error when writing logs to file.");
-      return;
-    }
-  }
-
+  map[new_size] = '\0';
   assert(estimate_tqueue_size(lines) == 0);
 
 CLEANUP:
   free_tqueue(lines);
+  msync(map, new_size + 1, MS_ASYNC);
 
-  if (status != -1 && (status = lockf(fd, F_ULOCK, 0)) == -1) {
-    write_log(LOG_WARN, "There was an error when writing logs to file. The lock cannot be removed.");
+  munmap(map, new_size + 1);
+  if (map == MAP_FAILED) {
+    switch (errno) {
+      CHECK_ERROR(ENOMEM, "Cannot write logs to file, out of memory.");
+      CHECK_ERROR(EAGAIN, "Cannot write logs to file, locked log file.");
+      CHECK_ERROR(ENFILE, "Cannot write logs to file, exceeded max opened files.");
+      CHECK_ERROR(ENODEV, "Cannot write logs to file, file system does not support memory mapping.");
+      CHECK_ERROR(EPERM,  "Cannot write logs to file, permission error.");
+    }
   }
 
   close(fd);
