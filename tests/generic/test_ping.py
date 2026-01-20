fee234ef4f30b4fc467fad50dfc950b8f312aa55
aloima <acar.aloima@gmail.com>
2026-01-20 13:56:38 +0300

    test: change library of ping test to tellypy

--- tests/generic/test_ping.py
+++ tests/generic/test_ping.py
@@ -1,6 +1,5 @@
 import unittest
-import redis
-from redis.exceptions import ResponseError
+from tellypy import Client, Kind
 import random
 from string import ascii_letters, digits
 
@@ -17,23 +16,28 @@ except ImportError:
 
 
 class PingCommand(unittest.TestCase):
+    client: Client
+
     @classmethod
     def setUpClass(self):
-        self.client = redis.Redis(host="localhost", port=6379, db=0)
+        self.client = Client(host="localhost", port=6379)
+        self.client.connect()
 
-    # If command respond b"PONG", returns True, otherwise returns False
     def test_without_arguments(self):
         response = self.client.execute_command("PING")
-        self.assertTrue(response)
+        self.assertEqual(response.kind, Kind.SIMPLE_STRING)
+        self.assertIsInstance(response.data, str)
+        self.assertEqual(response.data, "PONG")
 
-    # If command respond b"PONG", returns True, otherwise returns False
     def test_with_argument(self):
         value = "".join(random.choices(ascii_letters + digits, k=64))
-        response = self.client.execute_command("PING", value)
-        self.assertFalse(response)
+        response = self.client.execute_command(f"PING {value}")
+        self.assertEqual(response.kind, Kind.BULK_STRING)
+        self.assertIsInstance(response.data, str)
+        self.assertEqual(response.data, value)
 
     def test_with_arguments(self):
-        with self.assertRaises(ResponseError) as e:
-            self.client.execute_command("PING", "a", "b")
-
-        self.assertEqual(e.exception.args[0], wrong_argument("PING"))
+        response = self.client.execute_command("PING a b")
+        self.assertEqual(response.kind, Kind.SIMPLE_ERROR)
+        self.assertIsInstance(response.data, str)
+        self.assertEqual(response.data, wrong_argument("PING"))
