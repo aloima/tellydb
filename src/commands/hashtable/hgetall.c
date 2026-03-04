a58c6a3d6f6f858972e857befd7279ee5d8dcc1a
aloima <acar.aloima@gmail.com>
2026-03-05 00:12:31 +0300

    refactor: more readable HVALS and HGETALL commands

--- src/commands/hashtable/hgetall.c
+++ src/commands/hashtable/hgetall.c
@@ -5,6 +5,16 @@
 
 #include <gmp.h>
 
+static constexpr string_t c_null[] = {
+  [RESP2] = CREATE_STRING("$-1\r\n", 5),
+  [RESP3] = CREATE_STRING("_\r\n", 3)
+};
+
+static constexpr string_t c_bool[4][2] = {
+  [RESP2] = { [false] = CREATE_STRING("+false\r\n", 8), [true] = CREATE_STRING("+true\r\n", 7) },
+  [RESP3] = { [false] = CREATE_STRING("#f\r\n", 4), [true] = CREATE_STRING("#t\r\n", 4) },
+};
+
 static string_t run(struct CommandEntry *entry) {
   PASS_NO_CLIENT(entry->client);
 
@@ -29,107 +39,62 @@ static string_t run(struct CommandEntry *entry) {
   }
 
   const struct HashTable *table = kv->value;
+  const enum ProtocolVersion protover = entry->client->protover;
+
   char *response = entry->client->write_buf;
   uint64_t at;
 
-  switch (entry->client->protover) {
-    case RESP2: {
+  switch (protover) {
+    case RESP2:
       response[0] = '*';
       at = ltoa(table->size.used * 2, response + 1) + 1;
-      response[at++] = '\r';
-      response[at++] = '\n';
-
-      for (uint32_t i = 0; i < table->size.capacity; ++i) {
-        struct HashTableField *field = table->fields[i];
-
-        if (field) {
-          at += create_resp_string(response + at, field->name);
-
-          switch (field->type) {
-            case TELLY_NULL:
-              memcpy(response + at, "+null\r\n", 7);
-              at += 7;
-              break;
-
-            case TELLY_INT:
-              at += create_resp_integer_mpz(entry->client->protover, response + at, *((mpz_t *) field->value));
-              break;
-
-            case TELLY_DOUBLE:
-              at += create_resp_integer_mpf(entry->client->protover, response + at, *((mpf_t *) field->value));
-              break;
-
-            case TELLY_STR:
-              at += create_resp_string(response + at, *((string_t *) field->value));
-              break;
-
-            case TELLY_BOOL: {
-              if (*((bool *) field->value)) {
-                memcpy(response + at, "+true\r\n", 7);
-                at += 7;
-              } else {
-                memcpy(response + at, "+false\r\n", 8);
-                at += 8;
-              }
-            }
-
-            default:
-              break;
-          }
-        }
-      }
-
       break;
-    }
 
-    case RESP3: {
+    case RESP3:
       response[0] = '%';
       at = ltoa(table->size.used, response + 1) + 1;
-      response[at++] = '\r';
-      response[at++] = '\n';
+      break;
+  }
 
-      for (uint32_t i = 0; i < table->size.capacity; ++i) {
-        struct HashTableField *field = table->fields[i];
+  response[at++] = '\r';
+  response[at++] = '\n';
 
-        if (field) {
-          at += create_resp_string(response + at, field->name);
+  for (uint32_t i = 0; i < table->size.capacity; ++i) {
+    struct HashTableField *field = table->fields[i];
+    if (!field) continue;
 
-          switch (field->type) {
-            case TELLY_NULL:
-              memcpy(response + at, "_\r\n", 3);
-              at += 3;
-              break;
+    at += create_resp_string(response + at, field->name);
+    char *buf = response + at;
 
-            case TELLY_INT:
-              at += create_resp_integer_mpz(entry->client->protover, response + at, *((mpz_t *) field->value));
-              break;
-
-            case TELLY_DOUBLE:
-              at += create_resp_integer_mpf(entry->client->protover, response + at, *((mpf_t *) field->value));
-              break;
-
-            case TELLY_STR: {
-              at += create_resp_string(response + at, *((string_t *) field->value));
-              break;
-            }
-
-            case TELLY_BOOL: {
-              if (*((bool *) field->value)) {
-                memcpy(response + at, "#t\r\n", 4);
-              } else {
-                memcpy(response + at, "#f\r\n", 4);
-              }
-
-              at += 4;
-            }
-
-            default:
-              break;
-          }
-        }
+    switch (field->type) {
+      case TELLY_NULL: {
+        const string_t constant = c_null[protover];
+        memcpy(buf, constant.value, constant.len);
+        at += constant.len;
+        break;
       }
 
-      break;
+      case TELLY_INT:
+        at += create_resp_integer_mpz(protover, buf, *((mpz_t *) field->value));
+        break;
+
+      case TELLY_DOUBLE:
+        at += create_resp_integer_mpf(protover, buf, *((mpf_t *) field->value));
+        break;
+
+      case TELLY_STR:
+        at += create_resp_string(buf, *((string_t *) field->value));
+        break;
+
+      case TELLY_BOOL: {
+        string_t constant = c_bool[protover][*((bool *) field->value)];
+        memcpy(buf, constant.value, constant.len);
+        at += constant.len;
+        break;
+      }
+
+      default:
+        break;
     }
   }
 
