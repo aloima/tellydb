45f07f52790a446b392fce9fa4a765f545a36284
aloima <acar.aloima@gmail.com>
2026-03-08 18:22:12 +0300

    perf: add control flag to notifier

--- headers/utils/notifier.h
+++ headers/utils/notifier.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdatomic.h>
 
 #if defined(__linux__)
-  typedef int event_notifier_t;
+  typedef struct {
+    int efd;
+    _Atomic(bool) has;
+  } event_notifier_t;
 #elif defined(__APPLE__)
-  typedef int event_notifier_t[2];
+  typedef struct {
+    int fds[2];
+    _Atomic(bool) has;
+  } event_notifier_t;
 #endif
 
 event_notifier_t *create_notifier();
--- src/utils/notifier.c
+++ src/utils/notifier.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdatomic.h>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -16,35 +17,39 @@
     event_notifier_t *notifier = malloc(sizeof(event_notifier_t));
     if (VERY_UNLIKELY(notifier == NULL)) return NULL;
 
-    *notifier = eventfd(0, EFD_NONBLOCK);
-    if (VERY_UNLIKELY(*notifier == -1)) {
+    notifier->efd = eventfd(0, EFD_NONBLOCK);
+    if (VERY_UNLIKELY(notifier->efd == -1)) {
       free(notifier);
       return NULL;
     }
 
+    atomic_init(&notifier->has, false);
     return notifier;
   }
 
   void signal_notifier(event_notifier_t *notifier, uint64_t n) {
     if (VERY_UNLIKELY(notifier == NULL)) return;
-    write(*notifier, &n, sizeof(n));
+    write(notifier->efd, &n, sizeof(n));
+    atomic_store_explicit(&notifier->has, true, memory_order_relaxed);
   }
 
   uint64_t consume_notifier(event_notifier_t *notifier) {
     if (VERY_UNLIKELY(notifier == NULL)) return -1;
+    if (!atomic_load_explicit(&notifier->has, memory_order_relaxed)) return 0;
 
     uint64_t val = 0;
-    read(*notifier, &val, sizeof(val));
+    read(notifier->efd, &val, sizeof(val));
 
+    atomic_store_explicit(&notifier->has, false, memory_order_relaxed);
     return val;
   }
 
   int get_notifier(event_notifier_t *notifier) {
-    return *notifier;
+    return notifier->efd;
   }
 
   void destroy_notifier(event_notifier_t *notifier) {
-    close(*notifier);
+    close(notifier->efd);
     free(notifier);
   }
 #elif defined(__APPLE__)
@@ -55,30 +60,33 @@
     event_notifier_t *notifier = malloc(sizeof(event_notifier_t));
     if (VERY_UNLIKELY(notifier == NULL)) return NULL;
 
-    if (VERY_UNLIKELY(pipe(*notifier) == -1)) {
+    if (VERY_UNLIKELY(pipe(notifier->fds) == -1)) {
       free(notifier);
       return NULL;
     }
 
-    int flags = fcntl((*notifier)[0], F_GETFL, 0);
-    fcntl((*notifier)[0], F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(notifier->fds[0], F_GETFL, 0);
+    fcntl(notifier->fds[0], F_SETFL, flags | O_NONBLOCK);
 
+    atomic_init(&notifier->has, false);
     return notifier;
   }
 
   void signal_notifier(event_notifier_t *notifier, uint64_t n) {
     if (VERY_UNLIKELY(notifier == NULL)) return;
-    write((*notifier)[1], &n, sizeof(n));
+    write(notifier->fds[1], &n, sizeof(n));
+    atomic_store_explicit(&notifier->has, true, memory_order_relaxed);
   }
 
   uint64_t consume_notifier(event_notifier_t *notifier) {
     if (VERY_UNLIKELY(notifier == NULL)) return -1;
+    if (!atomic_load_explicit(&notifier->has, memory_order_relaxed)) return 0;
 
     uint64_t result = 0;
     uint64_t val[128];
     int bytes;
 
-    while ((bytes = read((*notifier)[0], val, sizeof(val))) > 0) {
+    while ((bytes = read(notifier->fds[0], val, sizeof(val))) > 0) {
       const int count = (bytes / sizeof(val[0]));
 
       for (int i = 0; i < count; ++i) {
@@ -90,12 +98,12 @@
   }
 
   int get_notifier(event_notifier_t *notifier) {
-    return (*notifier)[0];
+    return notifier->fds[0];
   }
 
   void destroy_notifier(event_notifier_t *notifier) {
-    close((*notifier)[0]);
-    close((*notifier)[1]);
+    close(notifier->fds[0]);
+    close(notifier->fds[1]);
     free(notifier);
   }
 #endif
