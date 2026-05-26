775f14837de833343f1b0f52a130676945274169
aloima <acar.aloima@gmail.com>
2026-05-26 18:39:38 +0300

    fix(database): add missing checking whether key is deleted or not

--- src/database/delete.c
+++ src/database/delete.c
@@ -7,11 +7,13 @@ bool delete_data(Database *database, string_t key) {
 
   // When deleted, value is not appeared, it is reachable
   const bool deleted = delete_from_hashtable(database->data, &key);
+  if (!deleted)
+    return false;
+
   free(kv->key.value);
   free_value(kv->value);
 
   free(kv);
-
   return true;
 }
 
