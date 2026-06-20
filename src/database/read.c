7718cd4351a13b3b750cc2599bd916b80e4ec728
aloima <acar.aloima@gmail.com>
2026-06-20 18:07:07 +0300

    refactor(database): add global structs to reduce argument count

--- src/database/read.c
+++ src/database/read.c
@@ -1,6 +1,25 @@
 #include <telly.h>
 
-static void collect_bytes(const int fd, char *block, const uint16_t block_size, uint16_t *at, const uint32_t count, void *data) {
+// Will be used for method arguments, includes everything which will be used except individual data such as string
+typedef struct GenericArguments {
+  const int fd;
+  char *block;
+  const uint16_t block_size;
+  uint16_t *at;
+} GenericArguments;
+
+typedef struct UnallocatedValue {
+  void **data;
+  const enum TellyTypes type;
+  uint32_t *element_count;
+} UnallocatedValue;
+
+static void collect_bytes(const GenericArguments *arguments, const uint32_t count, void *data) {
+  auto fd = arguments->fd;
+  auto block = arguments->block;
+  auto block_size = arguments->block_size;
+  auto at = arguments->at;
+
   uint32_t remaining = count;
 
   if ((*at + remaining) < block_size) {
@@ -24,14 +43,14 @@ static void collect_bytes(const int fd, char *block, const uint16_t block_size, 
   *at = remaining;
 }
 
-static size_t collect_string(string_t *string, const int fd, char *block, const uint16_t block_size, uint16_t *at, bool nt) {
+static size_t collect_string(const GenericArguments *arguments, string_t *string, bool nt) {
   string->len = 0;
 
   uint8_t first;
-  collect_bytes(fd, block, block_size, at, 1, &first);
+  collect_bytes(arguments, 1, &first);
 
   const uint8_t byte_count = (first >> 6);
-  collect_bytes(fd, block, block_size, at, byte_count, &string->len);
+  collect_bytes(arguments, byte_count, &string->len);
 
   string->len = ((string->len << 6) | (first & 0b111111));
 
@@ -42,20 +61,20 @@ static size_t collect_string(string_t *string, const int fd, char *block, const 
     string->value = malloc(string->len);
   }
 
-  collect_bytes(fd, block, block_size, at, string->len, string->value);
+  collect_bytes(arguments, string->len, string->value);
 
   return (1 + byte_count + string->len);
 }
 
-static size_t collect_integer(mpz_t *number, const int fd, char *block, const uint16_t block_size, uint16_t *at) {
+static size_t collect_integer(const GenericArguments *arguments, mpz_t *number) {
   uint8_t specifier;
-  collect_bytes(fd, block, block_size, at, 1, &specifier);
+  collect_bytes(arguments, 1, &specifier);
 
   const bool negative = (specifier & 0x80);
   const uint8_t byte_count = ((specifier & 0x7F) + 1);
 
   uint8_t data[byte_count];
-  collect_bytes(fd, block, block_size, at, byte_count, data);
+  collect_bytes(arguments, byte_count, data);
 
   mpz_t value;
   mpz_init(value);
@@ -80,9 +99,9 @@ static size_t collect_integer(mpz_t *number, const int fd, char *block, const ui
   return (1 + byte_count);
 }
 
-static size_t collect_double(mpf_t *number, const int fd, char *block, const uint16_t block_size, uint16_t *at) {
+static size_t collect_double(const GenericArguments *arguments, mpf_t *number) {
   uint8_t two[2];
-  collect_bytes(fd, block, block_size, at, 2, two);
+  collect_bytes(arguments, 2, two);
 
   uint8_t specifier = two[0], indicator = two[1];
   const bool negative = (specifier & 0x80);
@@ -90,7 +109,7 @@ static size_t collect_double(mpf_t *number, const int fd, char *block, const uin
   int16_t exp = ((indicator & 0x80) ? (indicator & 0x7F) : -1);
 
   uint8_t data[byte_count];
-  collect_bytes(fd, block, block_size, at, byte_count, data);
+  collect_bytes(arguments, byte_count, data);
 
   const bool is_even = (data[0] > 0x10);
 
@@ -146,87 +165,69 @@ static size_t collect_double(mpf_t *number, const int fd, char *block, const uin
   return (2 + byte_count);
 }
 
-typedef struct AllocationArguments {
-  const int fd;
-  char *block;
-  const uint16_t block_size;
-  uint16_t *at;
-  size_t *collected_bytes;
-  uint32_t *element_count;
-} AllocationArguments;
+static int allocate_value(const GenericArguments *arguments, const UnallocatedValue value, size_t *collected_bytes) {
+  const enum TellyTypes type = value.type;
+  void **data = value.data;
+  uint32_t *element_count = value.element_count;
 
-static inline AllocationArguments create_allocation_arguments(
-  const int fd, char *block, const uint16_t block_size, uint16_t *at, size_t *collected_bytes, uint32_t *element_count
-) {
-  AllocationArguments arguments = {
-    .fd = fd,
-    .at = at,
-    .block = block,
-    .block_size = block_size,
-    .collected_bytes = collected_bytes,
-    .element_count = element_count,
-  };
-
-  return arguments;
-}
-
-static int allocate_value(void **value, const enum TellyTypes type, AllocationArguments arguments) {
   if (type == TELLY_LIST || type == TELLY_HASHTABLE) {
-    collect_bytes(arguments.fd, arguments.block, arguments.block_size, arguments.at, 4, arguments.element_count);
+    collect_bytes(arguments, 4, element_count);
 
     if (type == TELLY_LIST) {
-      *(arguments.collected_bytes) += (4 + *(arguments.element_count)); // includes size bytes and type bytes of listnodes
-      *value = ll_create();
+      *collected_bytes += (4 + *element_count); // includes size bytes and type bytes of listnodes
+      *data = ll_create();
     } else if (type == TELLY_HASHTABLE) {
-      *(arguments.collected_bytes) += 5; // includes size bytes and last (0x17) byte
-      *value = create_hashtable(*(arguments.element_count), string_hash, string_compare);
+      *collected_bytes += 5; // includes size bytes and last (0x17) byte
+      *data = create_hashtable(*element_count, string_hash, string_compare);
     }
   } else {
     const int64_t size = PRIMARY_TYPE_SIZE_TABLE[type];
     ASSERT(size, !=, -1);
-    *value = malloc(size);
+    *data = malloc(size);
   }
 
-  if (*value == NULL)
+  if (*data == NULL)
     return -1;
 
   return 0;
 }
 
 // TODO: memory checking
-static size_t collect_kv(KeyValue *kv, const int fd, char *block, const uint16_t block_size, uint16_t *at) {
+static size_t collect_kv(const GenericArguments *arguments, KeyValue *kv) {
   string_t key;
   void *value = NULL;
   uint8_t type;
 
-  size_t collected_bytes = collect_string(&key, fd, block, block_size, at, false) + 1;
-  collect_bytes(fd, block, block_size, at, 1, &type);
+  size_t collected_bytes = collect_string(arguments, &key, false) + 1;
+  collect_bytes(arguments, 1, &type);
 
   const int64_t size = PRIMARY_TYPE_SIZE_TABLE[type];
   uint32_t element_count;
 
-  AllocationArguments arguments = create_allocation_arguments(fd, block, block_size, at, &collected_bytes, &element_count);
-  const int allocated = allocate_value(&value, type, arguments);
+  const int alloc_ret = ({
+    const UnallocatedValue unallocated_value = { .data = &value, .type = type, .element_count = &element_count };
+    allocate_value(arguments, unallocated_value, &collected_bytes);
+  });
 
-  if (allocated == -1) {
+  if (alloc_ret == -1) {
     // TODO
   }
 
   switch (type) {
     case TELLY_INT:
-      collected_bytes += collect_integer(value, fd, block, block_size, at);
+      collected_bytes += collect_integer(arguments, value);
       break;
 
     case TELLY_DOUBLE:
-      collected_bytes += collect_double(value, fd, block, block_size, at);
+      collected_bytes += collect_double(arguments, value);
       break;
 
     case TELLY_STR:
-      collected_bytes += collect_string(value, fd, block, block_size, at, false);
+      collected_bytes += collect_string(arguments, value, false);
       break;
 
     case TELLY_BOOL:
-      collect_bytes(fd, block, block_size, at, 1, value);
+      collect_bytes(arguments, 1, value);
       collected_bytes += 1;
       break;
 
@@ -235,41 +236,42 @@ static size_t collect_kv(KeyValue *kv, const int fd, char *block, const uint16_t
 
       while (true) {
         uint8_t byte;
-        collect_bytes(fd, block, block_size, at, 1, &byte);
+        collect_bytes(arguments, 1, &byte);
 
         if (byte == 0x17) {
           break;
         }
 
         NameValue *field = malloc(sizeof(NameValue));
-        collected_bytes += collect_string(&field->name, fd, block, block_size, at, true);
+        collected_bytes += collect_string(arguments, &field->name, true);
         field->value.type = byte;
         collected_bytes += 1; // type byte
 
         void **data = &field->value.data;
+        const int field_alloc_ret = ({
+          const UnallocatedValue unallocated_value = { .data = data, .type = field->value.type, .element_count = NULL };
+          allocate_value(arguments, unallocated_value, &collected_bytes);
+        });
 
-        AllocationArguments field_arguments = create_allocation_arguments(fd, block, block_size, at, &collected_bytes, NULL);
-        const int allocated = allocate_value(data, field->value.type, field_arguments);
-
-        if (allocated == -1) {
+        if (field_alloc_ret == -1) {
           // TODO
         }
 
         switch (field->value.type) {
           case TELLY_INT:
-            collected_bytes += collect_integer(*data, fd, block, block_size, at);
+            collected_bytes += collect_integer(arguments, *data);
             break;
 
           case TELLY_DOUBLE:
-            collected_bytes += collect_double(*data, fd, block, block_size, at);
+            collected_bytes += collect_double(arguments, *data);
             break;
 
           case TELLY_STR:
-            collected_bytes += collect_string(*data, fd, block, block_size, at, false);
+            collected_bytes += collect_string(arguments, *data, false);
             break;
 
           case TELLY_BOOL:
-            collect_bytes(fd, block, block_size, at, 1, *data);
+            collect_bytes(arguments, 1, *data);
             collected_bytes += 1;
             break;
 
@@ -289,12 +291,14 @@ static size_t collect_kv(KeyValue *kv, const int fd, char *block, const uint16_t
       for (uint32_t i = 0; i < size; ++i) {
         uint8_t byte;
         void *list_value = NULL;
-        collect_bytes(fd, block, block_size, at, 1, &byte);
+        collect_bytes(arguments, 1, &byte);
 
-        AllocationArguments list_value_arguments = create_allocation_arguments(fd, block, block_size, at, &collected_bytes, NULL);
-        const int allocated = allocate_value(&list_value, byte, list_value_arguments);
+        const int list_value_alloc_ret = ({
+          UnallocatedValue unallocated_value = { .data = &list_value, .type = byte, .element_count = NULL };
+          allocate_value(arguments, unallocated_value, &collected_bytes);
+        });
 
-        if (allocated == -1) {
+        if (list_value_alloc_ret == -1) {
           // TODO
         }
 
@@ -303,19 +307,19 @@ static size_t collect_kv(KeyValue *kv, const int fd, char *block, const uint16_t
             break;
 
           case TELLY_INT:
-            collected_bytes += collect_integer(list_value, fd, block, block_size, at);
+            collected_bytes += collect_integer(arguments, list_value);
             break;
 
           case TELLY_DOUBLE:
-            collected_bytes += collect_double(list_value, fd, block, block_size, at);
+            collected_bytes += collect_double(arguments, list_value);
             break;
 
           case TELLY_STR:
-            collected_bytes += collect_string(list_value, fd, block, block_size, at, false);
+            collected_bytes += collect_string(arguments, list_value, false);
             break;
 
           case TELLY_BOOL:
-            collect_bytes(fd, block, block_size, at, 1, list_value);
+            collect_bytes(arguments, 1, list_value);
             collected_bytes += 1;
             break;
 
@@ -340,11 +344,11 @@ static size_t collect_kv(KeyValue *kv, const int fd, char *block, const uint16_t
   return collected_bytes;
 }
 
-static size_t collect_database(Database **database, const int fd, char *block, const uint16_t block_size, uint16_t *at, uint64_t *count) {
-  collect_bytes(fd, block, block_size, at, 8, count);
+static size_t collect_database(const GenericArguments *arguments, Database **database, uint64_t *count) {
+  collect_bytes(arguments, 8, count);
 
   string_t name;
-  size_t collected_bytes = collect_string(&name, fd, block, block_size, at, true) + 8;
+  size_t collected_bytes = collect_string(arguments, &name, true) + 8;
 
   const uint64_t needed = pow(2, get_bit_count(*count));
   const uint64_t capacity = ((needed > DATABASE_INITIAL_SIZE) ? needed : DATABASE_INITIAL_SIZE);
@@ -354,7 +358,7 @@ static size_t collect_database(Database **database, const int fd, char *block, c
 
   for (uint64_t i = 0; i < *count; ++i) {
     KeyValue *kv = malloc(sizeof(KeyValue));
-    collected_bytes += collect_kv(kv, fd, block, block_size, at);
+    collected_bytes += collect_kv(arguments, kv);
 
     (void) insert_into_hashtable((*database)->data, &kv->key, kv);
   }
@@ -372,15 +376,16 @@ size_t read_file(const int fd, const off_t file_size, char *block, const uint16_
 
     off_t collected_bytes = at;
     uint64_t data_count = 0;
-    Database *database;
+    Database *database = NULL;
+
+    const GenericArguments arguments = { .fd = fd, .block = block, .block_size = block_size, .at = &at };
 
     do {
-      collected_bytes += collect_database(&database, fd, block, block_size, &at, &data_count);
+      collected_bytes += collect_database(&arguments, &database, &data_count);
       loaded_count += data_count;
 
-      if (database->id == hashed) {
+      if (database->id == hashed)
         set_main_database(database);
-      }
     } while (collected_bytes != file_size);
 
     if (!get_main_database()) {
