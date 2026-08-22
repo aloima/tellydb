6e545d1b4d13b5468240cc3b6563ff1bcd03a661
aloima <acar.aloima@gmail.com>
2026-08-22 17:58:25 +0300

    refactor(commands): reorganize `HELLO` command

--- src/commands/generic/hello.c
+++ src/commands/generic/hello.c
@@ -1,51 +1,52 @@
 #include <telly.h>
 
+static const string_t PROTOCOLS[] = {
+  CREATE_SIZED_STRING("unspecified"),
+  CREATE_SIZED_STRING("unspecified"),
+  CREATE_SIZED_STRING("RESP2"),
+  CREATE_SIZED_STRING("RESP3")
+};
+
+static inline bool determine_protover(enum ProtocolVersion *protover, const string_t value) {
+  if (SSTREQ(value, CREATE_SIZED_STRING("2"))) {
+    *protover = RESP2;
+    return true;
+  } else if (SSTREQ(value, CREATE_SIZED_STRING("3"))) {
+    *protover = RESP3;
+    return true;
+  }
+
+  return false;
+}
+
 static string_t run(struct CommandEntry *entry) {
   PASS_NO_CLIENT(entry->client);
 
-  const uint32_t arg_count = entry->args->count;
-
-  if (arg_count != 1) {
+  if (entry->args->count != 1) {
     return WRONG_ARGUMENT_ERROR("HELLO");
   }
 
-  const string_t protover = entry->args->data[0];
-
-  if (SSTREQ(protover, CREATE_SIZED_STRING("2"))) {
-    entry->client->protover = RESP2;
-  } else if (SSTREQ(protover, CREATE_SIZED_STRING("3"))) {
-    entry->client->protover = RESP3;
-  } else {
+  if (!determine_protover(&entry->client->protover, entry->args->data[0])) {
     return RESP_ERROR_MESSAGE("Invalid protocol version");
   }
 
   char client_id[11];
   const uint32_t client_id_len = ltoa(entry->client->id, client_id);
+  ASSERT(client_id_len, <=, 11U);
+
+  const enum ProtocolVersion protover = entry->client->protover;
 
   string_t values[4][2] = {
-    {{"server",    6}, {"telly",   5}},
-    {{"version",   7}, {VERSION,   sizeof(VERSION) - 1}},
-    {{"protocol",  8}},
-    {{"client id", 9}, {client_id, client_id_len}}
+    {CREATE_SIZED_STRING("server"),    CREATE_SIZED_STRING("telly")},
+    {CREATE_SIZED_STRING("version"),   CREATE_SIZED_STRING(VERSION)},
+    {CREATE_SIZED_STRING("protocol"),  (protover < 4) ? PROTOCOLS[protover] : PROTOCOLS[0]},
+    {CREATE_SIZED_STRING("client id"), CREATE_STRING(client_id, client_id_len)}
   };
-
-  static const string_t protocols[] = {
-    {"unspecified", 11},
-    {"unspecified", 11},
-    {"RESP2", 5},
-    {"RESP3", 5}
-  };
-
-  if (entry->client->protover < 4) {
-    values[2][1] = protocols[entry->client->protover];
-  } else {
-    values[2][1] = protocols[0];
-  }
 
   char *buf = entry->client->write_buf;
   uint32_t at;
 
-  switch (entry->client->protover) {
+  switch (protover) {
     case RESP2:
       memcpy(buf, "*8\r\n", 4);
       at = 4;
@@ -57,14 +58,10 @@ static string_t run(struct CommandEntry *entry) {
       break;
   }
 
-  at += create_resp_string(buf + at, values[0][0]);
-  at += create_resp_string(buf + at, values[0][1]);
-  at += create_resp_string(buf + at, values[1][0]);
-  at += create_resp_string(buf + at, values[1][1]);
-  at += create_resp_string(buf + at, values[2][0]);
-  at += create_resp_string(buf + at, values[2][1]);
-  at += create_resp_string(buf + at, values[3][0]);
-  at += create_resp_string(buf + at, values[3][1]);
+  for (size_t i = 0; i < 4; ++i) {
+    at += create_resp_string(buf + at, values[i][0]); // key
+    at += create_resp_string(buf + at, values[i][1]); // value
+  }
 
   return CREATE_STRING(buf, at);
 }
